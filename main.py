51ba97b6c24caf6f953a795ff7589ce43a2a9af8
Jessica Kincaid <4j.kincaid@gmail.com>
2018-10-28 20:20:47 -0500

    added templates, followed examples from videos and studios

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
 import cgi
-import os
-import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
-
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['DEBUG'] = True      
+# displays runtime errors in the browser, too
 
 @app.route("/")
 def index():
-    template = jinja_env.get_template('signup_form.html')
-    return template.render()
-
-@app.route("/hello", methods=['POST'])
-def hello():
-    first_name = request.form['password']
+    return render_template('index.html', username_error, passwd_error, vpasswd_error, email_error)
+@app.route("/signup", methods=['POST'])
+def signup():
+    password = request.form['password']
     template = jinja_env.get_template('password.html')
-    return template.render(name=passwd)
+    return template.render(password=passwd)
 
 @app.route('/validate-user')
 def display_signup_form():
@@ -44,6 +40,15 @@ def validate_():
     email_error =''
 
 ####### testing the try/except block ############
+if len(username) < 3 or len(username) > 20:
+    username_error = "Must be between 3 and 20 characters"
+
+for char in username:
+    if char == " ":
+        username_error = "Cannot Have Spaces"
+        break
+    else:
+        username_error = ""
 
     ######### 
     if len(password) <= 0:
@@ -59,11 +64,16 @@ def validate_():
             if passwd > 20 or passwd < 3:
                     passwd_error = 'Username and password must be between 3 and 20 characters.'
 
-    # if not is_integer(minutes):
-    #     minutes_error = 'Not a valid integer'
-    # else:
-    #     minutes= int(minutes)
-    #     if minutes > 59 or minutes < 0:
-                
+        if not name_error and not passwd_error and not email_error:
+        # redisplay the form with the error messages.
+        # Go ahead and insert 
+            return signup_form.format(name_error=name_error, passwd_error=passwd_error, email_error=email_error, username=username, passwd=passwd, email=email)
+            # This inserts into the placeholder variable username, password, and #email however if one of the values is invalid, it will still be in #there. If one of the {values} above is valid, it can stay in place #and the user doesn't have to re-enter the valid value and they can #focus on the one that's invalid.
+                # Each of those strings must still be empty to pass here
+                # So it gets a success! message.
+        else:
+            return 'Success!'
+        
+
 
 app.run()
