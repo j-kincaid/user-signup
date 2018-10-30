8c85aed63016755789d45bb5ccee05f9489d55e0
Jessica Kincaid <4j.kincaid@gmail.com>
2018-10-30 09:53:43 -0500

    replaced success with route to welcome.html

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, render_template
-import cgi
+import cgi, os, jinja2
 
 # TODO: Create a signup form with fields for Username, Password, Verify
 # Password, Optional email, and submit button
@@ -42,16 +42,17 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template('index.html', username_error, passwd_error, vpasswd_error, email_error)
+    return render_template('index.html')
+
 @app.route("/signup", methods=['POST'])
 def signup():
     password = request.form['password']
-    template = jinja_env.get_template('password.html')
-    return template.render(password=passwd)
+    template = jinja_env.get_template('index.html')
+    return template.render(password=password)
 
 @app.route('/validate-user')
-def display_signup_form():
-    return signup_form.format(username='', username_error='', passwd='', passwd_error='', email='', email_error='')
+def signup_form():
+    return signup_form.format(username='', username_error='', password='', password_error='', email='', email_error='')
 
 # ####### the try/except block ############
 # def is_valid(signup):
@@ -64,48 +65,49 @@ def display_signup_form():
 def validate_():
 
     username = request.form['username']
-    passwd = request.form['password']
+    password = request.form['password']
     email = request.form['email']
 
     name_error =''
-    passwd_error =''
+    password_error =''
     email_error =''
 
-####### testing the try/except block ############
-if len(username) < 3 or len(username) > 20:
-    username_error = "Must be between 3 and 20 characters"
+    ####### testing the try/except block ############
+    if len(username) < 3 or len(username) > 20:
+        username_error = "Must be between 3 and 20 characters"
 
-for char in username:
-    if char == " ":
-        username_error = "Cannot Have Spaces"
-        break
-    else:
-        username_error = ""
-
-    ######### 
-    if len(password) <= 0:
-        input_error = 'Please complete all fields.'
-    else: 
-        if passwd != vpasswd:
-            passwd_error = 'Passwords must match.'
-        else: 
-            username = int(username)
-            passwd = int(passwd)
-            if username > 20 or username < 3:
-                name_error = 'Username and password must be between 3 and 20 characters.'
-            if passwd > 20 or passwd < 3:
-                    passwd_error = 'Username and password must be between 3 and 20 characters.'
-
-        if not name_error and not passwd_error and not email_error:
-        # redisplay the form with the error messages.
-        # Go ahead and insert 
-            return signup_form.format(name_error=name_error, passwd_error=passwd_error, email_error=email_error, username=username, passwd=passwd, email=email)
-            # This inserts into the placeholder variable username, password, and #email however if one of the values is invalid, it will still be in #there. If one of the {values} above is valid, it can stay in place #and the user doesn't have to re-enter the valid value and they can #focus on the one that's invalid.
-                # Each of those strings must still be empty to pass here
-                # So it gets a success! message.
+    for char in username:
+        if char == " ":
+            username_error = "Cannot Have Spaces"
+            break
         else:
-            return 'Success!'
-        
+            username_error = ""
 
+        ######### 
+        if len(password) <= 0:
+            input_error = 'Please complete all fields.'
+        else: 
+            if password != vpassword:
+                password_error = 'Passwords must match.'
+            else: 
+                username = int(username)
+                password = int(password)
+                if username > 20 or username < 3:
+                    name_error = 'Username and password must be between 3 and 20 characters.'
+                if password > 20 or password < 3:
+                        password_error = 'Username and password must be between 3 and 20 characters.'
+
+            if not name_error and not password_error:
+            # redisplay the form with the error messages.
+            # Go ahead and insert 
+                return signup_form.format(name_error=name_error, password_error=password_error, email_error=email_error, username=username, password=password, email=email)
+                # This inserts into the placeholder variable username, password, and #email however if one of the values is invalid, it will still be in #there. If one of the {values} above is valid, it can stay in place #and the user doesn't have to re-enter the valid value and they can #focus on the one that's invalid.
+                    # Each of those strings must still be empty to pass here
+                    # So it gets a success! message.
+        
+@app.route('/welcome')
+def welcome():
+    render_template = request.args('welcome.html')
+    return render_template
 
 app.run()
