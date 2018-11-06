49dcc1462002e8b2e44ccbe0f3a9620aa42478b5
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-05 20:13:50 -0600

    deleted, added, replaced : with =

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import cgi
 import jinja2
 import os 
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-
-jinja_env = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(template_dir))
-
 # TODO:
 # If all input is valid, show a welcome page with a message of "Welcome, [username]!"
 
@@ -18,17 +13,17 @@ app = Flask(__name__)
 app.config['DEBUG'] = True      
 # displays runtime errors in the browser, too
 
-@app.route("/")
+@app.route('/')
 def index():
-    template = jinja_env.get_template('index.html')
-    return template.render()
+    return render_template('index.html')
 
-@app.route('/validate', methods=['POST'])
+@app.route('/', methods=['POST'])
 def validate_user():
     username = request.form['username']
     password = request.form['password']
     v_password = request.form['v_password']
     email = request.form['email']
+    
     username_error = ''
     password_error = ''
     v_password_error = ''
@@ -39,54 +34,42 @@ def validate_user():
 # # TODO:
 # # Check for errors and reject and render feedback:
 
-    for char in username:
-        if char == "":
-            username_error = "Username cannot have spaces"
-        if len(username) < 3 or len(username) > 20:
-            username_error = "Username must be between 3 and 20 characters"
-        else:
-            username_error = ""
+    if " " in username or len(username) < 3 or len(username) > 20 or "":
+        username_error = "Username cannot have spaces and must be between 3 and 20 characters"
+ 
+   # return render_template("index.html", username=username, username_error=username_error)
 
-    for char in password:
-        if len(password) > 20 or len(password) < 3:
-            password_error = 'Password must be between 3 and 20 characters.'
-        else:
-            password_error = ""
-        if password != v_password:
-    # password must match verified password
-            v_password_error: "Passwords must match."
-        else:
-            v_password_error = ""  
+    
+    if len(password) > 20 or len(password) < 3 or "" or " " in password:
+        password_error = 'Password must be between 3 and 20 characters.'
+    
+    if password != v_password:
+        # password must match verified password
+        v_password_error = "Passwords must match."
 # email must have:
     if email:
-        for char in email: 
-            if not ('@ 'in email):
-                email_error = "Email must contain @."
+        if not ('@ 'in email):
+            email_error = "Email must contain @."
 
-            if len(email) > 20 or len(email) < 3:
-                email_error = "Email must be between 3 and 20 characters."
+        if len(email) > 20 or len(email) < 3:
+            email_error = "Email must be between 3 and 20 characters."
 
-            if not ('.' in email):
-                email_error = "Email must contain a . ."
+        if not ('.' in email):
+            email_error = "Email must contain a . ."
 
-            if ' ' in email:
-                email_error = "Email must not contain spaces."
-            else: 
-                email_error = " "
+        if ' ' in email:
+            email_error = "Email must not contain spaces."
+    
+    if not username_error and not password_error and not v_password_error and not email_error:
+        return render_template("welcome.html", username=username, email=email)
     else:
-        if len(username_error)==0 and len(password_error) == 0 and len(email_error) == 0:
-            return render_template("welcome.html", username=username, password=password, v_password=v_password, email=email)
-        else:
-            # redisplay the form with the error messages.
-            return render_template("index.html", username=username, username_error=username_error, password_error=password_error, v_password_error=v_password_error, email_error=email_error)
+        # redisplay the form with the error messages.
+        return render_template("index.html", username=username, username_error=username_error, password_error=password_error, v_password_error=v_password_error, email_error=email_error)
 
             # Go ahead and insert 
             # This inserts into the placeholder variable username, password, and #email however if one of the values is invalid, it will still be in #there. If one of the {values} above is valid, it can stay in place #and the user doesn't have to re-enter the valid value and they can #focus on the one that's invalid.
             # Each of those strings must still be empty to pass here
             # So it gets a success! message.
 
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html ')
 
 app.run()
